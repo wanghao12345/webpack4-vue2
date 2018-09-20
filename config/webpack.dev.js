f30913a67f767b846cdf1cd5e17164a3acbc5a2f
winner <1750110604@qq.com>
2018-09-20 17:48:40 +0800

    首页起步

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -9,17 +9,24 @@ const htmlPlugin = require('html-webpack-plugin');
 // npm install --save-dev extract-text-webpack-plugin@next
 // package.json: "extract-text-webpack-plugin": "^4.0.0-beta.0",
 const extractTextPlugin = require('extract-text-webpack-plugin');
-// 配置路径
+// vue-loader15.*之后的版本都是需要伴生VueLoaderPlugin
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+
+// 配置上线路径
 var website = {
     publicPath: "http://localhost:8888/"
+}
+// 配置绝对路径
+var resolve = function(dir){
+    return path.join(__dirname, '..', dir);
 }
 
 module.exports = {
     mode: 'development',
     // 入口文件的配置项
     entry: {
-        main: './src/js/main.js',
-        main1: './src/js/main1.js'
+        main: './src/main.js'
     },
     // 出口文件的配置项
     output: {
@@ -30,6 +37,15 @@ module.exports = {
         // publicPath: 主要作用就是处理静态文件路径
         publicPath: website.publicPath
     },
+    // 配置绝对路径
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            '@': resolve('src'),
+            'styles': resolve('src/assets/styles')
+        }
+    },
+
     // 模块：例如解读CSS，图片如何转换，压缩
     module: {
         rules: [
@@ -75,6 +91,18 @@ module.exports = {
                     loader: 'babel-loader'
                 },
                 exclude: /node_modules/
+            },
+            // 解析vue后缀文件
+            {
+                test: /\.vue$/,
+                use:[{
+                    loader: "vue-loader"
+                }]
+            },
+            // 主要配置iconfont
+            {
+                test: /\.(woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?name=fonts/[name].[md5:hash:hex:7].[ext]',
             }
         ]
     },
@@ -91,7 +119,7 @@ module.exports = {
             // JS文件的存放地点 head, body
             inject: 'body',
             // 要打包的HTML模板路径和文件名称
-            template: './src/index.html',
+            template: './index.html',
             // 为了开发中JS有缓存效果，所以加入hash，这样可以有效避免缓存JS
             hash: true,
             // 对HTML文件进行压缩
@@ -105,7 +133,9 @@ module.exports = {
             },
             // 指定HTML需要加载的JS
             chunks: ['main', 'main1']
-        })
+        }),
+        // vue-loader需要的
+        new VueLoaderPlugin()
     ],
     // 配置webpack开发服务功能
     devServer: {
--- src/js/main.js
+++
@@ -1,9 +0,0 @@
-import css from '../css/index.css'
-
-alert('1234');
-
-const test = function () {
-    console.log('test es6');
-}
-
-test();
